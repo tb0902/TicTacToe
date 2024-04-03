ec9e71453a24b24d08d231c71e5d556180d728b9
tb0902 <tbruton417@gmail.com>
2024-04-03 15:18:15 -0400

    Some of the logic is a bit wrong :\

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,20 +4,75 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        int xCounter = 0;
+        int oCounter = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[j].equals('X')) {
+                    xCounter++;
+                } else {
+                    oCounter++;
+                }
+
+                }
+            }
+        if (xCounter > oCounter) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public Boolean isInFavorOfO() {
+        int xCounter = 0;
+        int oCounter = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[j].equals('O')) {
+                    xCounter++;
+                } else {
+                    oCounter++;
+                }
+
+            }
+        }
+        if (oCounter > xCounter) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public Boolean isInFavorOfO() {
-        return null;
-    }
 
     public Boolean isTie() {
-        return null;
-    }
+        int xCounter = 0;
+        int oCounter = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[j].equals('X')) {
+                    xCounter++;
+                } else {
+                    oCounter++;
+                }
+
+            }
+        }
+        if (xCounter == oCounter) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
 
     public String getWinner() {
         return null;
